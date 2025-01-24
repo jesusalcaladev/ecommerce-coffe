7eab6c3d74146c66a8ca27b110ab14882852d0df
development-hobby <jesusalcalarojas@gmail.com>
2025-01-23 23:41:10 -0400

    Add FeaturedProducts and BannerDiscount components to Home page

--- frontend-ecommerce/src/app/page.tsx
+++ frontend-ecommerce/src/app/page.tsx
@@ -1,9 +1,13 @@
+import BannerDiscount from '@/components/banner-discount'
 import CaroucelTextBanner from '@/components/caroucel-text-banner'
+import FeacturedProducts from '@/components/feactured-products'
 
 export default function Home() {
   return (
     <div>
       <CaroucelTextBanner />
+      <FeacturedProducts />
+      <BannerDiscount />
     </div>
   )
 }
