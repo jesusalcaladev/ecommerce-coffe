e0f42e5609304ef1bb54b3e1dd3ec699346c33f1
development-hobby <jesusalcalarojas@gmail.com>
2025-01-24 19:43:55 -0400

    Add BannerProducts component for promotional display with background image

---
+++ frontend-ecommerce/src/components/banner-products.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+
+export default function BannerProducts() {
+  return (
+    <>
+      <div className='mt-4 text-center'>
+        <p>Sumèrgete en una experiència ùnica</p>
+        <h4 className='mt-2 text-5xl font-extrabold uppercase'>
+          CafèExquisito
+        </h4>
+        <p className='my-2 text-lg'>Despierta tus sentidos con cada sorbo</p>
+        <Link href={'#'} className={buttonVariants()}>
+          Comprar
+        </Link>
+      </div>
+      <div
+        className='h-[350px] lg:h-[600px] bg-center bg-cover bg-no-repeat mt-5'
+        style={{
+          backgroundImage: 'url(/5.jpg)',
+        }}
+      ></div>
+    </>
+  )
+}
