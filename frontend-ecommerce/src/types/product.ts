de769fad10588510ba652a2a36b142a2d00185a4
development-hobby <jesusalcalarojas@gmail.com>
2025-01-23 23:40:38 -0400

    Add TypeScript types for Product and Image entities

---
+++ frontend-ecommerce/src/types/product.ts
@@ -0,0 +1,36 @@
+type Image = {
+  id: string
+  documentId: string
+  name: string
+  alternativeText: null
+  caption: null
+  width: number
+  height: number
+  formats: string[]
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  previewUrl: string
+  provider: string
+  provider_metadata: null
+  createdAt: string
+  updatedAt: string
+}
+
+export type Product = {
+  id: string
+  productName: string
+  description: string
+  images: Image[]
+  price: number
+  isFeatured: boolean
+  active: boolean
+  origin: string
+  taste: string
+  publishedAt: string
+  slug: string
+  createdAt: string
+  updatedAt: string
+}
