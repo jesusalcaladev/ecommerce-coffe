942b4b61b52822887cf7d253d535abae0ec8a4be
development-hobby <jesusalcalarojas@gmail.com>
2025-01-26 00:52:18 -0400

    Add product-related API functions for category and product retrieval

--- frontend-ecommerce/src/services/products.ts
+++ frontend-ecommerce/src/services/products.ts
@@ -11,3 +11,21 @@ export const getCategory = async () => {
   const response = await GET(url)
   return response.data
 }
+
+export const getCategoryProducts = async (slug: string) => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products?populate=*&filters[category][slug][$eq]=${slug}`
+  const response = await GET(url)
+  return response.data
+}
+
+export const getCategoryTop = async () => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/categories?populate=*`
+  const response = await GET(url)
+  return response.data.slice(0, 3)
+}
+
+export const getProductsField = async () => {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/content-type-builder/content-types/api::product.product`
+  const response = await GET(url)
+  return response.data
+}
