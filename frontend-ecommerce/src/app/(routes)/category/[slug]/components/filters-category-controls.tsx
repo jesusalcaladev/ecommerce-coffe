93852a70d373bc67c6484e432436058be67e35f4
development-hobby <jesusalcalarojas@gmail.com>
2025-01-27 12:05:58 -0400

    Remove unused FilterCategoryControls component

--- frontend-ecommerce/src/app/(routes)/category/[slug]/components/filters-category-controls.tsx
+++
@@ -1,9 +0,0 @@
-import FilterOrigin from './filter-origin'
-
-export default function FilterCategoryControls() {
-  return (
-    <div className='sm:w-[350px] sm:mt-5'>
-      <FilterOrigin />
-    </div>
-  )
-}
