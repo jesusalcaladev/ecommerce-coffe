e89a6986f2d7d1f5574d880771529513afc17f54
development-hobby <jesusalcalarojas@gmail.com>
2025-01-27 12:09:03 -0400

    Replace product origin and taste labels with Badge components in ProductCard

--- frontend-ecommerce/src/app/(routes)/category/[slug]/components/product-card.tsx
+++ frontend-ecommerce/src/app/(routes)/category/[slug]/components/product-card.tsx
@@ -3,6 +3,7 @@
 'use client'
 
 import IconButton from '@/components/icon-button'
+import { Badge } from '@/components/ui/badge'
 import {
   Carousel,
   CarouselContent,
@@ -28,12 +29,8 @@ export default function ProductCard({ product }: ProductCardProps) {
       className='relative p-2 transition-all duration-100 rounded-lg hover:shadow-md'
     >
       <div className='absolute flex items-center justify-between gap-3 px-2 z-[1] top-4'>
-        <p className='px-2 py-1 text-xs text-white bg-black rounded-full dark:bg-white dark:text-black w-fit'>
-          {product.taste}
-        </p>
-        <p className='px-2 py-1 text-xs text-white bg-yellow-900 rounded-full w-fit'>
-          {product.origin}
-        </p>
+        <Badge>{product.taste}</Badge>
+        <Badge variant={'destructive'}>{product.origin}</Badge>
       </div>
       <Carousel
         opts={{
