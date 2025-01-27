63a03fcced03fd343ca2c4b6fec494de328ceeeb
development-hobby <jesusalcalarojas@gmail.com>
2025-01-27 19:20:16 -0400

    refactor: import component ProductImageMinuature

--- frontend-ecommerce/src/app/(routes)/cart/components/cart-item.tsx
+++ frontend-ecommerce/src/app/(routes)/cart/components/cart-item.tsx
@@ -1,35 +1,21 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
 'use client'
+import ProductImageMinuature from '@/components/shared/product-image-minuature'
 import { Badge } from '@/components/ui/badge'
 import { useCart } from '@/hooks/use-cart'
 import { formatPrice } from '@/lib/format-prices'
 import { cn } from '@/lib/utils'
 import { Product } from '@/types/product'
 import { X } from 'lucide-react'
-import { useRouter } from 'next/navigation'
 
 interface CartItemProps {
   item: Product
 }
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
-
 export function CartItem({ item }: CartItemProps) {
   const { removeItem } = useCart()
-  const router = useRouter()
   return (
     <li className='flex py-6 border-b'>
-      <div
-        onClick={() => router.push(`/product/${item.slug}`)}
-        className='cursor-pointer'
-      >
-        <img
-          src={`${BACKEND_URL}${item.images[0].url}`}
-          alt={`${item.productName} to Image`}
-          className='size-24 rounded-md overflow-hidden sm:w-auto sm:h-32'
-        />
-      </div>
+      <ProductImageMinuature imagesUrl={item.images[0].url} slug={item.slug} />
       <div className='flex justify-between flex-1 px-6'>
         <div>
           <p className='text-lg font-bold'>{item.productName}</p>
