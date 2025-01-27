a54309bf12d4e44a814113503b15ab04890f94c5
development-hobby <jesusalcalarojas@gmail.com>
2025-01-27 19:18:02 -0400

    Add AccessoryPage component for displaying no available accessories message

---
+++ frontend-ecommerce/src/app/(routes)/accessory/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AccessoryPage() {
+  return (
+    <div className='max-w-4xl py-4 mx-auto sm:py-32 sm:px-24'>
+      <h2 className='text-center mx-auto w-full'>
+        No hay accesorios disponibles
+      </h2>
+    </div>
+  )
+}
