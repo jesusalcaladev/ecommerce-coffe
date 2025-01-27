da9520ab6dc2fd12dce90339e115d7c90738e6e9
development-hobby <jesusalcalarojas@gmail.com>
2025-01-27 12:08:26 -0400

    Add filter origin functionality to CategorySlugPage for product filtering

--- frontend-ecommerce/src/app/(routes)/category/[slug]/page.tsx
+++ frontend-ecommerce/src/app/(routes)/category/[slug]/page.tsx
@@ -2,31 +2,65 @@
 import { Separator } from '@/components/ui/separator'
 import { useGetProductsCategory } from '@/hooks/use-get-products-category'
 import { useParams } from 'next/navigation'
-import FilterCategoryControls from './components/filters-category-controls'
 import SkeletonSchema from '@/components/skeleton-schema'
 import ProductCard from './components/product-card'
+import { useEffect, useState } from 'react'
+import FilterOrigin from './components/filter-origin'
 
 export default function CategorySlugPage() {
   const params = useParams()
   const { data, isLoading } = useGetProductsCategory(params.slug as string)
+  const [filterOrigin, setFilterOrigin] = useState('')
+  const query = new URLSearchParams(window.location.search)
+
+  const handleFilterOrigin = (value: string) => {
+    query.set('origin', value)
+    window.history.replaceState(
+      null,
+      '',
+      `${window.location.pathname}?${query}`
+    )
+    setFilterOrigin(value)
+  }
+
+  useEffect(() => {
+    if (query.get('origin')) {
+      setFilterOrigin(query.get('origin') as string)
+    }
+  }, [])
+
+  const filtersProducts =
+    filterOrigin === ''
+      ? data
+      : data?.filter?.((product) => product.origin === filterOrigin)
+
   return (
     <div className='max-w-6xl py-4 mx-auto sm:py-16 sm:px-24'>
       {data !== null && !isLoading && (
         <h1 className='text-3xl font-medium'>
-          {data[0].category.categoryName}
+          {data[0]?.category?.categoryName}
         </h1>
       )}
       <Separator />
       <div className='sm:flex sm:justify-between'>
-        <FilterCategoryControls />
+        <div className='sm:w-[350px] sm:mt-5 p-6'>
+          <FilterOrigin onChangeFilter={handleFilterOrigin} />
+        </div>
         <div className='grid gap-5 mt-8 sm:grid-cols-2 md:grid-cols-3 md:gap-10'>
           {isLoading && <SkeletonSchema grid={3} />}
           {}
           {!isLoading &&
-            data !== null &&
-            data.map((product) => (
+            filtersProducts !== null &&
+            filtersProducts?.map?.((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
+          {filtersProducts !== null &&
+            !isLoading &&
+            filtersProducts?.length === 0 && (
+              <p className='text-xl mx-auto w-full text-center'>
+                No hay productos con este filtro.
+              </p>
+            )}
         </div>
       </div>
     </div>
