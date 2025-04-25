b15114a0fd9da3b2ef7b5217d1d8a6d60777dff7
fluwdev <jesusalcalarojas@gmail.com>
2025-04-25 19:44:14 -0400

    feat: wrap Providers component with SessionProvider for authentication context

--- frontend-ecommerce/src/components/providers.tsx
+++ frontend-ecommerce/src/components/providers.tsx
@@ -3,13 +3,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import { ThemeProvider } from './theme-provider'
+import { SessionProvider } from 'next-auth/react'
 
 const queryClient = new QueryClient()
 
 export default function Providers({ children }: React.PropsWithChildren) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='system'>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute='class' defaultTheme='system'>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
