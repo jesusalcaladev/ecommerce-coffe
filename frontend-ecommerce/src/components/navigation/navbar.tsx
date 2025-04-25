415e27d1b90d803e852fad80d042d30281b095ea
fluwdev <jesusalcalarojas@gmail.com>
2025-04-25 19:44:25 -0400

    feat: enhance Navbar component with user authentication and profile image display

--- frontend-ecommerce/src/components/navigation/navbar.tsx
+++ frontend-ecommerce/src/components/navigation/navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
 import Link from 'next/link'
@@ -6,13 +7,16 @@ import MenuList from './menu-list'
 import ItemsMenuMobile from './items-menu-mobile'
 import { ModeToggle } from '../mode-toogle'
 import { useCart } from '@/hooks/use-cart'
+import { signIn, useSession } from 'next-auth/react'
+import { Button } from '../ui/button'
 
 export default function Navbar() {
   const { items } = useCart()
+  const { data: session, status } = useSession()
   return (
     <header className='flex items-center justify-between p-4 mx-auto cursor-pointer sm:max-4xl md:max-w-6xl'>
       <Link href={'/'}>
-        <h1 className='text-2xl'>
+        <h1 className='text-2xl font-semibold'>
           fluw
           <span className='font-bold text-orange-700'>Coffee</span>
         </h1>
@@ -38,9 +42,19 @@ export default function Navbar() {
         <Link href={'/loved-products'}>
           <Heart strokeWidth={1.25} className='cursor-pointer' />
         </Link>
-        {/* <Link href={'/user'}>
-          <User strokeWidth={1.25} className='cursor-pointer' />
-        </Link> */}
+        {status === 'authenticated' ? (
+          <Link href={'/user'}>
+            <img
+              src={session?.user?.image ?? ''}
+              className='rounded-full size-8'
+              alt='Profile Picture'
+            />
+          </Link>
+        ) : (
+          <Button onClick={() => signIn()} variant={'secondary'}>
+            Iniciar Session
+          </Button>
+        )}
         <ModeToggle />
       </div>
     </header>
