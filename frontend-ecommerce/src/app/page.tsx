329db6fe578954d6ae304510e7af2e676b911135
fluwdev <jesusalcalarojas@gmail.com>
2025-04-25 19:43:05 -0400

    feat: restructure Home component layout and enhance styling

--- frontend-ecommerce/src/app/page.tsx
+++ frontend-ecommerce/src/app/page.tsx
@@ -1,16 +1,20 @@
 import BannerDiscount from '@/components/banner-discount'
 import BannerProducts from '@/components/banner-products'
-import CaroucelTextBanner from '@/components/caroucel-text-banner'
 import ChooseCategory from '@/components/choose-category'
 import FeacturedProducts from '@/components/feactured-products'
+import HeroSection from '@/components/hero-section'
 
 export default function Home() {
   return (
-    <div>
-      <CaroucelTextBanner />
-      <FeacturedProducts />
+    <div className='bg-white dark:bg-neutral-950'>
+      <HeroSection />
+      <div id='featured'>
+        <FeacturedProducts />
+      </div>
       <BannerDiscount />
-      <ChooseCategory />
+      <div className='bg-gray-50 dark:bg-neutral-950 py-8'>
+        <ChooseCategory />
+      </div>
       <BannerProducts />
     </div>
   )
